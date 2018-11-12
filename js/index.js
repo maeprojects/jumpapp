27480d1ef76744ecacf58753d9584f9318d346eb
fedesimeon <fede.simeon@gmail.com>
2018-11-12 20:38:50 +0100

    Tutorial js files, Moving player
    I've added the two tutorials of both versions (2 and 3) of phaser. Added also the phaser2.js and phaser3.js libraries.
    Then i've modified phaser3-tutorial to make the player move, with the camera following the player.

--- js/index.js
+++ js/index.js
@@ -1,194 +1,24 @@
-var config = {
-	type: Phaser.AUTO, //Choose between Phaser.CANVAS and Phaser.WEBGL
-	width: 800,
-	height: 600,
-	physics: {
-		default: 'arcade',
-		arcade: {
-				gravity: { y: 300 },
-				debug: false
-		}
-	},
-	scene: {
-		preload: preload,
-		create: create,
-		update: update
-	}
-};
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update, render: render });
 
-var game = new Phaser.Game(config);
+function preload() {
 
-var score = 0;
-var scoreText;
-var gameOver = false;
+	game.load.baseURL = 'http://examples.phaser.io/assets/';
+    game.load.crossOrigin = 'anonymous';
 
-function preload ()
-{
-	this.load.image('sky', 'assets/sky.png');
-	this.load.image('ground', 'assets/platform.png');
-	this.load.image('star', 'assets/star.png');
-	this.load.image('bomb', 'assets/bomb.png');
-	this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 }); //As a sprite sheet, not an image. That is because it contains animation frames. 
+    game.load.image('phaser', 'sprites/phaser-dude.png');
+
 }
 
-function create ()
-{
-	this.add.image(400, 300, 'sky'); //Add the image to the scene: the position is relative to the center of the image
-	//this.add.image(0, 0, 'sky').setOrigin(0, 0) //Set the anchor of the image to the top left and position it to the top left of the scene
-	
-	/*In Arcade Physics there are two types of physics bodies: Dynamic and Static. 
-	A dynamic body is one that can move around via forces such as velocity or acceleration. 
-	It can bounce and collide with other objects and that collision is influenced by the mass 
-	of the body and other elements.
+function create() {
 
-	In stark contrast, a Static Body simply has a position and a size. It isn't touched by gravity, 
-	you cannot set velocity on it and when something collides with it, it never moves. 
-	Static by name, static by nature. And perfect for the ground and platforms that we're going to let the player run around on.
+    var sprite = game.add.sprite(0, 0, 'phaser');
 
-	But what is a Group? As their name implies they are ways for you to group together similar objects and control 
-	them all as one single unit. You can also check for collision between Groups and other game objects. 
-	Groups are capable of creating their own Game Objects via handy helper functions like create. 
-	A Physics Group will automatically create physics enabled children, saving you some leg-work in the process.
-	*/
-	
-	platforms = this.physics.add.staticGroup();
-	
-	platforms.create(400,568, 'ground').setScale(2).refreshBody();
-	
-	platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
-	
-
-	
-	//Creation of the player
-	player = this.physics.add.sprite(100, 420, 'dude'); //positioned at 100 x 450 pixels from the top of the game
-	
-	player.setBounce(0.2);
-	player.setCollideWorldBounds(true);
-	player.body.setGravityY(300); //Set gravity as "acceleration"
-	
-	
-	//Animations Setting
-	this.anims.create({
-		key: 'left',
-		frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-		frameRate: 10, //10 frames per second
-		repeat: -1 //loop=true
-	});
-
-	this.anims.create({
-		key: 'turn',
-		frames: [ { key: 'dude', frame: 4 } ],
-		frameRate: 20
-	});
-
-	this.anims.create({
-		key: 'right',
-		frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-		frameRate: 10,
-		repeat: -1
-	});
-	
-	
-	
-	//Creation of colliders btw player and game field
-	this.physics.add.collider(player, platforms);	
-	
-	
-	//Adding Stars
-	stars = this.physics.add.group({
-		key: 'star', //First it sets the texture key to be the star image
-		repeat: 11, //Then it sets the repeat value to be 11. Because it creates 1 child automatically, repeating 11 times means we'll get 12 in total
-		setXY: { x: 12, y: 0, stepX: 70 }
-	});
-
-	stars.children.iterate(function (child) {
-
-		child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
-
-	});
-	
-	
-	//Collider for stars and platforms
-	this.physics.add.collider(stars, platforms);
-	
-	//Overlap btw player and stars: call function: collectStar
-	this.physics.add.overlap(player, stars, collectStar, null, this);
-	
-	
-	
-	//Adding score
-	scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-	scoreText.setText('score: ' + score);
-	
-	
-	//Adding Bombs
-	bombs = this.physics.add.group();
-	this.physics.add.collider(bombs, platforms);
-	this.physics.add.collider(player, bombs, hitBomb, null, this);
 }
 
-function hitBomb(player, bomb){
-	this.physics.pause();
+function update() {
 
-    player.setTint(0xff0000);
-
-    player.anims.play('turn');
-
-    gameOver = true;	
 }
 
-function collectStar (player, star)
-{
-    star.disableBody(true, true);
-	score += 10;
-    scoreText.setText('score: ' + score);
-	
-	if (stars.countActive(true) === 0)
-    {
-        stars.children.iterate(function (child) {
+function render() {
 
-            child.enableBody(true, child.x, 0, true, true);
-
-        });
-
-        var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
-
-        var bomb = bombs.create(x, 16, 'bomb');
-        bomb.setBounce(1);
-        bomb.setCollideWorldBounds(true);
-        bomb.setVelocity(20, 0);
-        bomb.allowGravity = false;
-
-    }
-}
-
-function update ()
-{
-	//Create cursors to move the player with arrows
-	cursors = this.input.keyboard.createCursorKeys();
-	if (cursors.left.isDown)
-	{
-		player.setVelocityX(-160);
-
-		player.anims.play('left', true);
-	}
-	else if (cursors.right.isDown)
-	{
-		player.setVelocityX(160);
-
-		player.anims.play('right', true);
-	}
-	else
-	{
-		player.setVelocityX(0);
-
-		player.anims.play('turn');
-	}
-
-	if (cursors.up.isDown && player.body.touching.down)
-	{
-		player.setVelocityY(-480);
-	}
 }
