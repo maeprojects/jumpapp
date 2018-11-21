37e7527bc0ed43823dfb3057c7bdc8a426f0d73c
fedesimeon <fede.simeon@gmail.com>
2018-11-21 22:19:15 +0100

    Changes after Professor suggests
    Changed approach to move the world: now the platforms are moving, no more cycling creation of new moving worlds.
    Changed the function to detect collisions: now it's based on player.body.touching.down

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
+
+//Settings
 var resolution = [800, 600]
-var gravity = 330;
+var gravity = 850;
 
 var config = {
 	type: Phaser.AUTO,
@@ -26,14 +28,17 @@ var scoreText;
 var gameOver = false;
 var cursors = 0;
 var card;
-var startingPoint = false;
+var landed = false;
 var worldWidth = 0;
 var gameContext;
+var platformTouched;
+var player;
 
 function preload ()
 {
 	this.load.image('sky', 'assets/sky.png');
-	this.load.image('ground', 'assets/platform.png');
+	this.load.image('ground', 'assets/ground.png');
+	this.load.image('platform', 'assets/platform.png');
 	this.load.image('star', 'assets/star.png');
 	this.load.image('bomb', 'assets/bomb.png');
 	this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
@@ -41,12 +46,10 @@ function preload ()
 
 function create ()
 {
-	//this.add.image(-400, 300, 'sky');
 	this.add.image(400, 300, 'sky');
-	this.add.image(1200, 300, 'sky');
 	
 	
-	this.physics.world.setBounds(0, 0, 1600, 600);
+	this.physics.world.setBounds(0, 0, 800, 600);
 	worldWidth = this.physics.world.bounds.width;
 	
 	
@@ -54,12 +57,9 @@ function create ()
 	platforms = this.physics.add.staticGroup();
 	
 	platforms.create(400,568, 'ground').setScale(2).refreshBody();
-	platforms.create(-400,568, 'ground').setScale(2).refreshBody();
-	platforms.create(1200,568, 'ground').setScale(2).refreshBody();
 	
-	platforms.create(600, 400, 'ground');
-    platforms.create(750, 220, 'ground');
-	platforms.create(1200, 300, 'ground');
+	randomLevel = platforms.create(900, Phaser.Math.Between(50, 420), 'platform');
+	randomLevel.note = 3
 	
 
 	//Player Creation
@@ -68,43 +68,34 @@ function create ()
 	player.setBounce(0.2);
 	player.setCollideWorldBounds(true);
 	player.body.setGravityY(300);
-	player.setDepth(50);
-	
 	
 	//Player Animations Creation
 	this.anims.create({
 		key: 'right',
-		frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-		frameRate: 10,
+		frames: this.anims.generateFrameNumbers('dude', { start: 3, end: 8 }),
+		frameRate: 15,
 		repeat: -1 //loop=true
 	});
 
 	this.anims.create({
 		key: 'turn',
 		frames: [ { key: 'dude', frame: 4 } ],
-		frameRate: 20
+		frameRate: 5
 	});
 		
 	
 	//Creation of colliders btw player and game field
-	this.physics.add.collider(player, platforms);
+	this.physics.add.collider(player, platforms, platformsColliderCallback);
 		
 	
 	//Adding score
 	scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 	scoreText.setText('score: ' + score);
+	scoreText.setScrollFactor(0);
 	
 	
 	//Create cursors to move the player with arrows
 	cursors = this.input.keyboard.createCursorKeys();
-
-	
-    //Set Camera Bounds
-    this.cameras.main.setBounds(-300, 0, 5000, 0);
-	
-    //Make the camera follow the player
-    this.cameras.main.startFollow(player);
-	this.cameras.main.setFollowOffset(-300, 0);
 	
 	//Needed to use THIS on the following functions
 	gameContext = this;
@@ -112,46 +103,44 @@ function create ()
 
 function update ()
 {
+	platforms.getChildren().forEach(function(p){
+		if(p.note != undefined){
+			p.x = p.x - 3;
+			p.body.x = p.body.x - 3;
+		}
+	})
+	
 	if (cursors.up.isDown && player.body.touching.down)
 	{
-		player.setVelocityY(-480);
+		player.setVelocityY(-680);
 	}
 	
-	if(startingPoint) {
-		player.setVelocityX(130);
+	if(player.body.touching.down) {
 		player.anims.play('right', true);
-	} else if(player.getBottomLeft().y == 536){
-		startingPoint = true;
-	} else {
+	}	
+	else {
 		player.anims.play('turn', true);
+		platformTouched = false;
 	}
 	
-	
-	if(player.getBottomLeft().x > worldWidth-760 && player.getBottomLeft().x < worldWidth-755){
-		updateWorld();
+	if(randomLevel.x < 1000-randomLevel.width){
+		randomLevel = platforms.create(1000, Phaser.Math.Between(50, 420), 'platform');
+		randomLevel.note = 3;
 	}
 }
 
-function updateWorld () {
-	
-	gameContext.add.image(worldWidth+400, 300, 'sky');
-	gameContext.add.image(worldWidth+1200, 300, 'sky');
-	gameContext.physics.world.setBounds(0, 0, worldWidth+1600, 600);	
-	gameContext.cameras.main.setBounds(-300, 0, worldWidth+3000, 0);
-	
-	platforms.create(worldWidth+400,568, 'ground').setScale(2).refreshBody();
-	platforms.create(worldWidth+1200,568, 'ground').setScale(2).refreshBody();
-	
-	platforms.create(worldWidth+600, 400, 'ground');
-    platforms.create(worldWidth+850, 220, 'ground');
-	platforms.create(worldWidth+1200, 300, 'ground');
-	
-	//Update width
-	worldWidth = gameContext.physics.world.bounds.width;
+function platformsColliderCallback () {
+	if(!platformTouched && player.body.touching.down) {
+		console.log("Collide Event");
+		score++;
+		scoreText.setText('score: ' + score);
+	}
+	platformTouched = true;
 }
 
 document.onclick = function () {
 	if(player.body.touching.down){
-		player.setVelocityY(-480);
+		player.setVelocityY(-680);
+		platformTouched = false;
 	}
 }
