053f6a72b2a2c5bda12151a4c1fe1e027fdc3a0f
Marco <marco5.olivieri@mail.polimi.it>
2018-11-28 16:08:45 +0100

    Semplified code
    Comment some part of code that isn't useful to our purpose

--- tuner-precise/app/pitchDetector/app.js
+++ tuner-precise/app/pitchDetector/app.js
@@ -1,8 +1,8 @@
 const Application = function() {
   this.tuner = new Tuner()
   this.notes = new Notes('.notes', this.tuner)
-  this.meter = new Meter('.meter')
-  this.frequencyBars = new FrequencyBars('.frequency-bars')
+  //this.meter = new Meter('.meter')
+  //this.frequencyBars = new FrequencyBars('.frequency-bars')
   this.update({ name: 'A', frequency: 440, octave: 4, value: 69, cents: 0 })
 }
 
@@ -25,7 +25,7 @@ Application.prototype.start = function() {
   })
 
   if (!/Android/i.test(navigator.userAgent)) {
-    this.updateFrequencyBars()
+    //this.updateFrequencyBars()
   }
 }
 
@@ -34,12 +34,12 @@ Application.prototype.updateFrequencyBars = function() {
     this.tuner.analyser.getByteFrequencyData(this.frequencyData)
     this.frequencyBars.update(this.frequencyData)
   }
-  requestAnimationFrame(this.updateFrequencyBars.bind(this))
+  //requestAnimationFrame(this.updateFrequencyBars.bind(this))
 }
 
 Application.prototype.update = function(note) {
   this.notes.update(note)
-  this.meter.update((note.cents / 50) * 45)
+  //this.meter.update((note.cents / 50) * 45)
 }
 
 // noinspection JSUnusedGlobalSymbols
