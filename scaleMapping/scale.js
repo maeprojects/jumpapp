402ea548ced4387e33d37455eee7a928ae285496
Marco <marco5.olivieri@mail.polimi.it>
2018-11-29 12:23:53 +0100

    Added NoteFreq array

--- scaleMapping/scale.js
+++ scaleMapping/scale.js
@@ -14,3 +14,26 @@ function newNote(musicalNote){
   //CALL graphicsModule
   jumpLevel(level)
 }
+
+// calulate tones from C2 to A5
+numTones = 49
+var tones = []
+for(i=0; i<numTones; i++){
+  freq = 55*Math.pow(2,1/12)**i
+  tones[i] = Number(Math.round(freq+'e2')+'e-2'); // round at second decimals
+}
+
+
+var noteFreq = []  //array with the notation note as index and the relative frequency as value
+var noteTuples = []
+octave = 1
+letters = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"]
+
+//notes = {440: "A", 550:"B" hash map
+for(i=0; i<tones.length; i++){
+  if(i%12 == 3)
+    octave++
+  noteLetter = letters[i%12] + octave
+  noteTuples[i] = [noteLetter, tones[i]] // -> array di tuple [nota, frequenza] per mantenere l'ordine
+  noteFreq[noteLetter] = tones[i]
+}
