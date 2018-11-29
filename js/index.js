e44b37fd8237bb762ad87728a6d64d596530e281
fedesimeon <fede.simeon@gmail.com>
2018-11-29 11:15:58 +0100

    JumpArea, gameVelocity Dependency
    Setted new variables to give the possibility to the player to jump only in a specific area and trying to set the jump intensity dependent to the game velocity.

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ var resolution = [800, 600]
 var gravity = 850;
 var playerGravity = 400;
 var gameVelocity = 1;
-var levelsNumber = 8;
+var numberOfLevels = 8;
 
 //Game Configuration
 var config = {
@@ -28,49 +28,55 @@ var config = {
 var game = new Phaser.Game(config);
 
 //Game Variables
-var score = 0;
+var score;
 var scoreText;
-var gameOver = false;
-var restartScene = false;
-var cursors = 0;
-var card;
-var startedGame = false;
-var pausedGame = false;
+
+var cursors;
+
+var startedGame;
+var pausedGame;
+var restartScene;
+var goAhead;
 var platformTouched;
-var player;
+var gameOver;
+
 var levelsFieldHeight;
-var levels = [];
-var platformVelocity = 0;
-var playerWidth = 32;
-var playerHeight = 48;
-var playerLeftOffset = 200;
-var playerInitialY = playerHeight/2;
-var nextNote = 0;
-var currentNote = 0;
-var collider;
-var goAhead = true;
-var keysPressed = false;
+var levels;
+var platformVelocity;
+
+var player;
+var playerWidth;
+var playerHeight;
+var playerFixedX;
+var playerInitialY;
+
+var nextNote;
+var currentNote;
+
+
+var correctKeyPressed;
+var jumpArea;
+var jumpAreaWidth;
 
 function preload ()
 {	
 	this.load.image('background', 'assets/background.png');
-	this.load.image('ground', 'assets/ground.png');
 	this.load.image('platform', 'assets/platform.png');
-	this.load.image('star', 'assets/star.png');
-	this.load.image('bomb', 'assets/bomb.png');
 	this.load.image('gameover', 'assets/gameover.png');
 	this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: playerWidth, frameHeight: playerHeight });
 }
 
 function create ()
 {	
+	initVariables();
+	
 	this.add.image(resolution[0]/2, resolution[1]/2, 'background').setAlpha(0.2);
 	
 	
 	this.physics.world.setBounds(0, 0, resolution[0], resolution[1]);
 	
 	//Player Creation
-	player = this.physics.add.sprite(playerLeftOffset, playerInitialY, 'dude');
+	player = this.physics.add.sprite(playerFixedX, playerInitialY, 'dude');
 	
 	player.setCollideWorldBounds(false);
 	player.body.setGravityY(-gravity);
@@ -82,7 +88,7 @@ function create ()
 	//First Platform creation
 	
 		//List of levels generation
-		for (i = 1; i <= levelsNumber; i++) { 
+		for (i = 1; i <= numberOfLevels; i++) { 
 			levels[i-1] = i;
 		}
 		
@@ -93,7 +99,7 @@ function create ()
 		newRandomLevel = generateRandomLevel();
 		levelValue = newRandomLevel[0];
 		levelHeight = newRandomLevel[1];
-		randomLevel = platforms.create(350, levelHeight, 'platform');
+		randomLevel = platforms.create(349, levelHeight, 'platform');
 		randomLevel.note = levelValue;
 		
 		
@@ -110,29 +116,53 @@ function create ()
 	//Player Animations Creation
 	this.anims.create({
 		key: 'run',
-		frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-		frameRate: 15,
+		frames: this.anims.generateFrameNumbers('dude', { start: 1, end: 9 }),
+		frameRate: 15*Math.sqrt(gameVelocity),
 		repeat: -1 //loop=true
 	});
 
 	this.anims.create({
 		key: 'stop',
-		frames: [ { key: 'dude', frame: 4 } ],
+		frames: [ { key: 'dude', frame: 1 } ],
 		frameRate: 5
 	});
 		
 	
 	//Creation of colliders btw player and game field
-	collider = this.physics.add.collider(player, platforms, platformsColliderCallback);
+	this.physics.add.collider(player, platforms, platformsColliderCallback);
 		
 	
 	//Adding score
-	scoreText = this.add.text(16, 16, 'score:       click to start', { fontSize: '32px', fill: '#000' });
+	scoreText = this.add.text(16, 16, 'score:       Enter/Space to start', { fontSize: '32px', fill: '#000' });
 	scoreText.setScrollFactor(0);
 	
 	
 	//Create cursors to move the player with arrows
 	cursors = this.input.keyboard.createCursorKeys();
+}
+
+function initVariables() {
+	cursors = 0;
+	
+	score = 0;
+	gameOver = false;
+	restartScene = false;
+	startedGame = false;
+	pausedGame = false;
+	goAhead = true;
+	jumpArea = false;
+	levels = [];
+	platformVelocity = 0;
+	playerWidth = 32;
+	playerHeight = 48;
+	playerFixedX = 200;
+	playerInitialY = playerHeight/2;
+	nextNote = 0;
+	currentNote = 0;
+	
+	correctKeyPressed = true;
+	
+	jumpAreaWidth = playerWidth+30*gameVelocity;
 }
 
 function update ()
@@ -143,19 +173,35 @@ function update ()
 				p.x = p.x - platformVelocity;
 				p.body.x = p.body.x - platformVelocity;
 				if(p.x < -p.width-1000) p.destroy();
-				if((p.x-p.width/2) >= (playerLeftOffset-player.width/2)-gameVelocity && (p.x-p.width/2) < (playerLeftOffset-player.width/2)) {
-					currentNote = p.note;
-				}
-				if((p.x-p.width/2)-p.width >= (playerLeftOffset-player.width/2)-gameVelocity && (p.x-p.width/2)-p.width < (playerLeftOffset-player.width/2)) {
+				
+				playerLeftBorder = (playerFixedX-player.width/2);
+				platformLeftBorder = (p.x-p.width/2);
+				platformWidth = p.width;
+				
+				changedCurrentPlatform = platformLeftBorder >= playerLeftBorder-gameVelocity && platformLeftBorder < playerLeftBorder;
+				changedNextPlatform = platformLeftBorder-platformWidth >= playerLeftBorder-gameVelocity && platformLeftBorder-platformWidth < playerLeftBorder;
+				
+				//Change Platform event to set nextNote
+				if(changedNextPlatform) {
 					nextNote = p.note;
-					keysPressed = false;
 					console.log("Current Note: ", currentNote);
 					console.log("Next Note: ", nextNote);
 				}
-				if(!keysPressed && ((p.x+p.width/2) >= (playerLeftOffset-player.width/2)-gameVelocity && (p.x+p.width/2) < (playerLeftOffset-player.width/2))) {
+				
+				
+				
+				//Current Platform Changed Event
+				if(changedCurrentPlatform && !correctKeyPressed) { //decide if the player has to die or not, depending on correctKeyPressed
 					goAhead = false;
-					console.log(p.x+p.width/2);
-					console.log(playerLeftOffset+player.width);
+				} else if(changedCurrentPlatform) { //Set currentNote
+							currentNote = p.note;
+							jumpArea = false;
+							correctKeyPressed = false;
+						}
+				
+				//jumpArea Setter when the player enter the jumpArea
+				if(platformLeftBorder+platformWidth-jumpAreaWidth >= playerLeftBorder-gameVelocity && platformLeftBorder+platformWidth-jumpAreaWidth < playerLeftBorder) {
+					jumpArea = true;
 				}
 			}
 		})
@@ -185,7 +231,7 @@ function update ()
 				this.add.image(resolution[0]/2, resolution[1]/2, 'gameover');
 				gameOver = true;
 				console.log("GameOver: ",gameOver);
-				scoreText.setText('score: ' + score + '    click to restart');
+				scoreText.setText('score: ' + score + '    Enter/Space to restart');
 			}
 		}
 		
@@ -200,38 +246,13 @@ function update ()
 		}
 	} else {
 		player.anims.play('stop', true);
-		scoreText.setText('score: ' + score + ' Game Paused, click to resume...');
-	}
-}
-
-document.onclick = function canvasClick() {
-	if(!startedGame){
-		player.body.setGravityY(playerGravity);
-		scoreText.setText('score: ' + score);
-		startedGame = true;
-		console.log("startedGame: ",startedGame);
-	} else if(!gameOver){
-		pausedGame = !pausedGame;
-		scoreText.setText('score: ' + score);
-		console.log("PausedGame: ",pausedGame);
-	}
-		
-	if(gameOver) {
-		player.y = -100; //Move the player and hide it
-		score = 0;
-		restartScene = true;
-		goAhead = true;
-		console.log("restartScene: ",restartScene);
-		gameOver = false;
-		console.log("gameOver: ",gameOver);
-		startedGame = false;
-		console.log("startedGame: ",startedGame);
+		scoreText.setText('score: ' + score + ' Game Paused, Enter/Space to resume...');
 	}
 }
 
 var generateRandomLevel = function() {
 	randomLevelValue = Math.floor(Math.random()*(levels.length))+1;
-	currentLevelHeight = (((levelsNumber+1)-randomLevelValue)*(levelsFieldHeight/levelsNumber))+((levelsFieldHeight/levelsNumber)/2)+(player.height-(levelsFieldHeight/levelsNumber));
+	currentLevelHeight = (((numberOfLevels+1)-randomLevelValue)*(levelsFieldHeight/numberOfLevels))+((levelsFieldHeight/numberOfLevels)/2)+(player.height-(levelsFieldHeight/numberOfLevels));
 
 	return [randomLevelValue, currentLevelHeight];
 }
@@ -246,43 +267,101 @@ function platformsColliderCallback () {
 }
 
 document.onkeydown = function(event) {
-	if((!event.repeat) && player.body.touching.down && !pausedGame) {
-		if(event.key == nextNote-currentNote+1) { //Go up
-			goAhead = true;
-			keysPressed = true;
-			switch(event.key) {
-				case "1":
-					player.setVelocityY(-250); //OK
-					break;
-				case "2":
-					player.setVelocityY(-450); //OK
-					break;
-				case "3":
-					player.setVelocityY(-600); //OK
-					break;
-				case "4":
-					player.setVelocityY(-750); //OK
-					break;
-				case "5":
-					player.setVelocityY(-830); //OK
-					break;
-				case "6":
-					player.setVelocityY(-950); //OK
-					break;
-				case "7":
-					player.setVelocityY(-1000); //OK
-					break;
-				case "8":
-					player.setVelocityY(-1090); //OK
-					break;
-				default:
-					break;
+	if(!event.repeat){
+		if(event.key == "Enter" || event.key == " "){
+			
+			if(!startedGame){
+				player.body.setGravityY(playerGravity);
+				scoreText.setText('score: ' + score);
+				startedGame = true;
+				console.log("startedGame: ",startedGame);
+			} else if(!gameOver){
+				pausedGame = !pausedGame;
+				scoreText.setText('score: ' + score);
+				console.log("PausedGame: ",pausedGame);
 			}
-		} else if (event.key == currentNote-nextNote+1) { //Go down
-					player.setVelocityY(-300); //OK
-					keysPressed = true;
-					goAhead = true;
-				} 
-				else goAhead = false;
+				
+			if(gameOver) {
+				player.y = -100; //Move the player and hide it
+				score = 0;
+				restartScene = true;
+				goAhead = true;
+				console.log("restartScene: ",restartScene);
+				gameOver = false;
+				console.log("gameOver: ",gameOver);
+				startedGame = false;
+				console.log("startedGame: ",startedGame);
+				correctKeyPressed = true;
+				console.log("correctKeyPressed: ",correctKeyPressed);
+			}
+		
+		} else if(player.body.touching.down && !pausedGame && jumpArea) {
+					jumpRatio = nextNote-currentNote+1;
+					jumpKey = String(jumpRatio);
+					if(event.key == nextNote && currentNote<=nextNote) { //Go up
+						goAhead = true;
+						correctKeyPressed = true;
+						switch(jumpKey) {
+							case "1":
+								player.setVelocityY(-250*Math.pow(gameVelocity, 1/3)); //OK
+								break;
+							case "2":
+								player.setVelocityY(-450*Math.pow(gameVelocity, 1/3)); //OK
+								break; 
+							case "3":
+								player.setVelocityY(-600*Math.pow(gameVelocity, 1/6)); //OK
+								break;
+							case "4":
+								player.setVelocityY(-750*Math.pow(gameVelocity, 1/15)); //OK
+								break;
+							case "5":
+								player.setVelocityY(-830*Math.pow(gameVelocity, 1/17)); //OK
+								break;
+							case "6":
+								player.setVelocityY(-950*Math.pow(gameVelocity, 1/18)); //OK
+								break;
+							case "7":
+								player.setVelocityY(-1000*Math.pow(gameVelocity, 1/19)); //OK
+								break;
+							case "8":
+								player.setVelocityY(-1090*Math.pow(gameVelocity, 1/20)); //OK
+								break;
+							default:
+								break;
+						}
+						/* switch(jumpKey) {
+							case "1":
+								player.setVelocityY(-1*Math.pow(250*gameVelocity, 1/3)); //OK
+								break;
+							case "2":
+								player.setVelocityY(-1*Math.pow(450*gameVelocity, 1/3)); //OK
+								break; 
+							case "3":
+								player.setVelocityY(-1*Math.pow(600*gameVelocity, 1/3)); //OK
+								break;
+							case "4":
+								player.setVelocityY(-1*Math.pow(750*gameVelocity, 1/3)); //OK
+								break;
+							case "5":
+								player.setVelocityY(-1*Math.pow(830*gameVelocity, 1/3)); //OK
+								break;
+							case "6":
+								player.setVelocityY(-1*Math.pow(950*gameVelocity, 1/3)); //OK
+								break;
+							case "7":
+								player.setVelocityY(-1*Math.pow(1000*gameVelocity, 1/3)); //OK
+								break;
+							case "8":
+								player.setVelocityY(-1*Math.pow(1090*gameVelocity, 1/3)); //OK
+								break;
+							default:
+								break;
+						} */
+					} else if (event.key == nextNote) { //Go down
+								player.setVelocityY(-300); //OK
+								correctKeyPressed = true;
+								goAhead = true;
+							}
+		}
 	}
 }
