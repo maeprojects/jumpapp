9298386cea8c261601153c2e1934523d07881235
Marco <marco5.olivieri@mail.polimi.it>
2018-12-12 16:40:31 +0100

    Added scaleMapping
    Added methods to change the pitch based on the scale reference and the note reference

--- scaleMapping/scale.js
+++ scaleMapping/scale.js
@@ -1,35 +1,81 @@
-
-
+var noteReference = "C3"
+var scaleReference = levelScaleColorsMatrix[0][1]
+var currentScale = getCurrentScale()
 
 /*
 * this function is called from the pitchDetector Module when a new note is detected
+* musicalNote = a note with its octave: ex C#3
 */function newNote(musicalNote){
   	level = 0
-  	
+
   	switch(musicalNote){
-    case "C3": level = 1; break;
-    case "D3": level = 2; break;
-    case "E3": level = 3; break;
-    case "F3": level = 4; break;
-    case "G3": level = 5; break;
-    case "A3": level = 6; break;
-    case "B3": level = 7; break;
-    case "C4": level = 8; break;
+    case currentScale[0]: level = 1; break;
+    case currentScale[1]: level = 2; break;
+    case currentScale[2]: level = 3; break;
+    case currentScale[3]: level = 4; break;
+    case currentScale[4]: level = 5; break;
+    case currentScale[5]: level = 6; break;
+    case currentScale[6]: level = 7; break;
+    case currentScale[7]: level = 8; break;
+    default: level = 0; break;
   	}
+
+    console.log(musicalNote)
+    //console.log(level)
+
   	//CALL graphicsModule
   	jumpLevel(level)
 }
 
+//calucate the current scale based on the note and scale reference in setting
+function getCurrentScale(){
+    currentScale =[]
+    index=0
+    extractLetterReference = noteReference.substring(0, noteReference.length-1)
+    extractOctaveReference = parseInt(noteReference.substring(noteReference.length-1))
+
+    // calculate the scale
+    j=letters.indexOf(extractLetterReference)
+    for(i=0; i<scaleReference.length; i++){
+      if(scaleReference[i]==1){
+        currentScale[index] = letters[j]
+        index++
+      }
+      j++
+      if(j%12==0)
+        j=0
+    }
 
 
+    // update the correct octave
+    changeOctave = false
+    currentScale[0] += extractOctaveReference
+    for(i=1; i<currentScale.length; i++){
+      if((currentScale[i] == "C" || currentScale[i] == "C#") && !changeOctave){
+        extractOctaveReference++
+        changeOctave = true
+      }
+      currentScale[i] += extractOctaveReference
+    }
 
+    return currentScale;
 
+}
+
+/* function called from Graphics module
+* numLevelGame is in the range [0 - 6]
+*/
+function changeGameLevel(numLevelGame){
+  if(numLevelGame < levelScaleColorsMatrix.length)
+    scaleReference = levelScaleColorsMatrix[numLevelGame][1]
+  currentScale = getCurrentScale()
+}
 
 //------------------------------------------
 // function for debug
 
 function changeReference(note){
-	NoteReference = note;
+	noteReference = note;
 }
 
 function buttonPlayReference(){
